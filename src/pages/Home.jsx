7dcc6c5967ff3161ab6c713bcbca9b74b08806af
Ryan Garza <ryangarza@live.com>
2020-11-25 00:46:35 -0600

    update text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -49,7 +49,7 @@ const Home = (props) => {
             <div className="contact-container" id="contact">
 
                 <p>Thank you for visiting my portfolio site. Please contact me by using one of the links below.
-                I'm always interested in discussing new and exciting collaboration opportunities. I also enjoy connecting
+                I'm always interested in discussing new and exciting opportunities. I also enjoy connecting
                 others, so if I can be of assistance to you in your networking in any way, please feel free to reach out!
                 </p>
 
