b11a094260101aefb044394c1012e75a8c8e852b
Ryan Garza <ryangarza@live.com>
2020-11-24 23:23:31 -0600

    css updates and screenshot

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,26 +4,25 @@ const Footer = (props) => {
     return (
   
   <div>
-  <footer>
-    <p className="contact-me">CONTACT AND FOLLOW ME<span class="full-stop-white">:</span></p>
-    <div className="container">
-      <div className="row footer-row">
-        <div className="col-4 linkedin-link">
-          <a href="https://www.linkedin.com/in/ryangarza/">LINKEDIN</a>
-        </div>
-        <div className="col-4 github-link">
-          <a href="https://github.com/rgarzadev">GITHUB</a>
-        </div>
-        <div className="col-4 email-link">
-          <a href="mailto:ryangarza@live.com">EMAIL</a>
+    <footer>
+      <p className="contact-me">CONTACT AND FOLLOW ME<span class="full-stop-white">:</span></p>
+      <div className="container">
+        <div className="row footer-row">
+          <div className="col-4 linkedin-link">
+            <a href="https://www.linkedin.com/in/ryangarza/">LINKEDIN</a>
+          </div>
+          <div className="col-4 github-link">
+            <a href="https://github.com/rgarzadev">GITHUB</a>
+          </div>
+          <div className="col-4 email-link">
+            <a href="mailto:ryangarza@live.com">EMAIL</a>
+          </div>
         </div>
       </div>
-    </div>
-  </footer>
-  <div className="row">
-      <div className="col copyright">&copy;2020 RYAN GARZA DEV</div>
-    </div>
+    </footer>
+    <div className="col copyright">&copy;2020 RYAN GARZA DEV</div>
   </div>
+  
     )
 }
 
