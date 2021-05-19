8c80888f61ba5a7319fdf84e74f77e45326c3aba
Ryan Garza <ryangarza@live.com>
2021-05-18 20:19:21 -0500

    npm audit fix

--- src/data/project-data.jsx
+++ src/data/project-data.jsx
@@ -3,72 +3,83 @@ const projectData = [
         url: "https://polar-escarpment-17006.herokuapp.com",
         img: "poke-memory-screen.png",
         title: "Pokemon Memory Game",
-        description: "An app that hides pairs of random Pokemon characters under 24 Poké Balls. Use your memory abilities to match each pair before time runs out!"
+        description: "An app that hides pairs of random Pokemon characters under 24 Poké Balls. Use your memory abilities to match each pair before time runs out!",
+        repository: "https://www.google.com"
         
     },
     {
         url: "https://vast-hollows-62316.herokuapp.com",
         img: "react-directory-screen-1.png",
         title: "React Directory Search",
-        description: "The goal of this project was to create a React Employee Directory Search app that allows users to search or sort directory results by last name."
+        description: "The goal of this project was to create a React Employee Directory Search app that allows users to search or sort directory results by last name.",
+        repository: "https://www.google.com"
         
     },
     {
         url: "https://aqueous-mesa-60429.herokuapp.com",
         img: "budget-screen.PNG",
         title: "MongoDB PWA Budget Tracker",
-        description: "The goal of this project was to create a PWA budget tracker that allows users to save their transaction data on or offline and to track their budget and expenses."
+        description: "The goal of this project was to create a PWA budget tracker that allows users to save their transaction data on or offline and to track their budget and expenses.",
+        repository: "https://www.google.com"
         
     },
     {
         url: "https://polar-escarpment-17006.herokuapp.com",
         img: "wt-dashboard-screen.PNG",
         title: "MongoDB Workout Tracker",
-        description: "The goal of this project was to create a workout tracker app that allows users to save their workout activities and track their fitness progress."
+        description: "The goal of this project was to create a workout tracker app that allows users to save their workout activities and track their fitness progress.",
+        repository: "https://www.google.com"
         
     },
     {
         url: "https://guarded-meadow-74931.herokuapp.com",
         img: "burger-app-screenshot.PNG",
         title: "Virtual Burger App",
-        description: 'The goal of this project was to create a virtual burger eating app that allows users to add, "devour", and delete their favorite burgers.'
+        description: 'The goal of this project was to create a virtual burger eating app that allows users to add, "devour", and delete their favorite burgers.',
+        repository: "https://www.google.com"
         
     },
     {
         url: "https://still-wave-13932.herokuapp.com",
         img: "note-taker-screen.PNG",
         title: "Express Note Taking App",
-        description: "The goal of this project was to create an Express web-based note taking app that saves and recalls note data for the user."        
+        description: "The goal of this project was to create an Express web-based note taking app that saves and recalls note data for the user."   ,
+        repository: "https://www.google.com"     
     },
     {
         url: "https://github.com/rgarzadev/Employee-Data-CLI",
         img: "emp-data-cli-screen.PNG",
         title: "Employee Data CLI",
-        description: "The goal of this project was to create a MySQL employee database command line interface using inquirer that allows users to view and alter selected employee information."        
+        description: "The goal of this project was to create a MySQL employee database command line interface using inquirer that allows users to view and alter selected employee information.",
+        repository: "https://www.google.com"        
     },
     {
         url: "https://github.com/rgarzadev/Team-Generator",
         img: "team-generator-screen.PNG",
         title: "Node CLI Roster Generator",
-        description: "The goal of this project was to create a Node CLI Engineer team generator that outputs an HTML file based on user command line inputs."        
+        description: "The goal of this project was to create a Node CLI Engineer team generator that outputs an HTML file based on user command line inputs.",
+        repository: "https://www.google.com"        
     },
     {
         url: "https://rgarzadev.github.io/Weather-Dashboard",
         img: "weather-dashboard-screen.PNG",
         title: "Weather Dashboard",
-        description: "The goal of this project was to create a weather dashboard that allows users to search by city name and view current and 5-day forecast information. Previously searched cities are saved and can be recalled by user by clicking buttons in list."        
+        description: "The goal of this project was to create a weather dashboard that allows users to search by city name and view current and 5-day forecast information. Previously searched cities are saved and can be recalled by user by clicking buttons in list.",
+        repository: "https://www.google.com"        
     },
     {
         url: "https://rgarzadev.github.io/Workday-Planner",
         img: "workday-planner-screen.PNG",
         title: "Moment.JS Workday Planner",
-        description: "The goal of this project was to create a browswer based workday planner that displays the current time and date using Moment.JS, and that allows users to store agenda items to local storage and recall them when refreshing or reopening the browswer."        
+        description: "The goal of this project was to create a browswer based workday planner that displays the current time and date using Moment.JS, and that allows users to store agenda items to local storage and recall them when refreshing or reopening the browswer.",
+        repository: "https://www.google.com"        
     },
     {
         url: "https://github.com/penguinorange/fuzzy_feelings",
         img: "fuzzy-feelings-screen.PNG",
         title: "Agile Development Team Project",
-        description: "Fuzzy Feelings is an app that displays motivational and/or animal memes depending on the user's choice. It is a FUN application designed to uplift the user according to their preferences."
+        description: "Fuzzy Feelings is an app that displays motivational and/or animal memes depending on the user's choice. It is a FUN application designed to uplift the user according to their preferences.",
+        repository: "https://www.google.com"
     },
 
 
